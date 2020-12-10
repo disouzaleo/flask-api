53c744ae9addfc719ebf0c269593a251c2bf58e0
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2020-12-09 23:06:18 -0300

    Implementing the 404 error endpoint

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import abort, Flask
 
 flask_app = Flask('flask-api')
 
 @flask_app.route('/')
 def index():
     return ''
+
+@flask_app.route('/error')
+def get_one_error():
+    abort(404, description="Resource not found")
--- tests.py
+++ tests.py
@@ -2,16 +2,22 @@ import unittest
 from app import flask_app
 
 class TestHome(unittest.TestCase):
+    """This class introduces API test case"""
 
     def setUp(self):
-        app = flask_app.test_client()
-        self.response = app.get('/')
+        self.client = flask_app.test_client
 
     def test_get(self):
+        response = self.client().get('/')
         self.assertEqual(200, response.status_code)
 
     def test_content_type(self):
+        response = self.client().get('/')
         self.assertIn('text/html', response.content_type)
+
+    def test_error_404(self):
+        response = self.client().get('/error')
+        self.assertEqual(404, response.status_code)
 
 if __name__ == '__main__':
     unittest.main()
