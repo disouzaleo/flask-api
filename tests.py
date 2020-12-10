74a1f25353f2137dfc7b331f99f735a537e05f46
Leonardo Rodrigues Lima <lrodlima@gmail.com>
2020-12-09 22:16:47 -0300

    Adding a fail test for a success 200 endpoint

--- tests.py
+++ tests.py
@@ -1,16 +1,12 @@
 import unittest
-from app import fatorial
+from app import flask_app
 
-class TestFatorial(unittest.TestCase):
+class TestHome(unittest.TestCase):
 
-    def test_fatorial(self):
-        self.assertEqual(fatorial(0), 1)
-        self.assertEqual(fatorial(1), 1)
-        self.assertEqual(fatorial(2), 2)
-        self.assertEqual(fatorial(3), 6)
-        self.assertEqual(fatorial(4), 24)
-        self.assertEqual(fatorial(5), 120)
-        self.assertEqual(fatorial(6), 720)
+    def test_get(self):
+        app = flask_app.test_client()
+        response = app.get('/')
+        self.assertEqual(200, response.status_code)
 
 if __name__ == '__main__':
     unittest.main()
